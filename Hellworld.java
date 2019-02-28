9660d69c9f7b9c2d29b154cada254395b9e9e9b8
Zapovil <zapzknt@gmail.com>
2019-02-28 16:39:00 +0530

    commit update

--- Hellworld.java
+++ Hellworld.java
@@ -1,7 +1,7 @@
 public class Helloworld{
 
-	pulic static void main(String args[]){
+	public static void main(String args[]){
 	
-		Sstem.out.println("Hello Zapovil_asdsad
+		Sstem.out.println("Hello Zapovil");
 	}
 }
